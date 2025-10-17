1220d143e4fe6f395bf8b1a415a3faf84a75a362
Mujeeb raji <mujeebraji48@gmail.com>
2025-10-17 16:10:19 +0100

    Add rolling animation to dice images

---
+++ Dice-roller.js
@@ -0,0 +1,17 @@
+function rollDice() {
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceImage = document.getElementById("diceImage");
+    const diceResult = document.getElementById("diceResult");
+    const values = [];
+    const images = [];
+
+    for (let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) +  1;
+        console.log(value);
+        values.push(value);
+        images.push(`<img src="dice_images/Dice-${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImage.innerHTML = images.join('');
+}
